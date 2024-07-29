66e5f1d6e1af1f3ccab99523e0c9b402ffafa4be
jonnald0430 <hernandez29@my.smccd.edu>
2024-07-29 00:17:55 -0700

    added score, collision (game over) and movement mechanics for snake

--- main.py
+++ main.py
@@ -47,4 +47,52 @@ class SnakeFood:
         Canvas.create_oval(x,y,x + SPACE_SIZE, y +
                            SPACE_SIZE, fill= FOOD,
                            tag="food")
-        
+
+# collision and movement        
+def next_turn(snake, food):
+    # gets x,y coordinates of snake (first element of coordinates list for snake)
+    x, y = Snake.coordinates[0]
+
+    # checks for direction of snake
+    if direction == "up":
+        y -= SPACE_SIZE
+    elif direction == "down":
+        y += SPACE_SIZE
+    elif direction == "left":
+        x -= SPACE_SIZE
+    elif direction == "right":
+        x += SPACE_SIZE
+
+    # new x,y coordinates inserted at [0] of Snake.coordinates
+    Snake.coordinates.insert(0, (x,y))
+
+    # new rectangle is created on canvas object of new x,y coordinates, color specified by fill=SNAKE
+    square = Canvas.create_rectangle(
+        x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE)
+    
+    # new rectangle added to method Snake.squares
+    Snake.squares.insert(0, square)
+
+    # checks if head of snake is at the same coordinates of food object 
+    if x == SnakeFood.coordinates[0] and y == SnakeFood.coordinates[1]:
+        # score increments by 1 if at same coordinates
+        global score
+        score += 1
+        # points is printed, food is deleted as snake eats it
+        Label.config(text="Points:{}".format(score))
+        Canvas.delete("food")
+        food = SnakeFood()
+
+    # if food isn't eaten then tail of snake is removed from coordinates and squares list
+    else:
+        del Snake.coordinates[-1]
+        Canvas.delete(Snake.squares[-1])    
+        del Snake.squares[-1]
+
+    # if snake runs into wall or itself, game over function is called
+    if check_collisions(snake):
+        game_over()
+
+    # if not, then next_turn is called again and game keeps going
+    else:
+        window.after(SPEED, next_turn, snake, food)
